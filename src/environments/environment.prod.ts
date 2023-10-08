7d50b484ca92715d6ec3d506c0231dc51b756387
Benjamin SIMON <bensim.602@gmail.com>
2023-10-08 12:49:04 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB01Wa2FoOmOgoH3lv537uEGpAdR8sRDgY",
+    authDomain: "project-4082772279023332108.firebaseapp.com",
+    projectId: "project-4082772279023332108",
+    storageBucket: "project-4082772279023332108.appspot.com",
+    messagingSenderId: "1025657268756",
+    appId: "1:1025657268756:web:97859fc35281f58c541501"
+  }
+};
